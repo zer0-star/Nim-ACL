d00354c6892008056ff83ffcd37d6c80fbc3bbd6
chaemon <chaemon@gmail.com>
2020-09-16 20:20:15 +0900

    modintのinitModIntが消えていたので修正

--- src/nim_acl/modint.nim
+++ src/nim_acl/modint.nim
@@ -32,16 +32,6 @@ when not defined ATCODER_MODINT_HPP:
       static: assert false
   template umod*[T:ModInt](self: T):uint32 = self.type.umod
 
-#  proc initModInt*(v: SomeInteger or ModInt; M: static[int] = 1_000_000_007): auto {.inline.} =
-#    when v is ModInt: return v
-#    else:
-#      if 0 <= v:
-#        if v < M: return ModInt[M](v.uint32)
-#        else: return ModInt[M]((v mod M).uint32)
-#      else:
-#        var v = v mod M
-#        if v < 0: v += M
-#        return ModInt[M](v.uint32)
   proc init*[T:ModInt](t:typedesc[T], v: SomeInteger or T): auto {.inline.} =
     when v is T: return v
     else:
@@ -53,7 +43,10 @@ when not defined ATCODER_MODINT_HPP:
         v = v mod T.umod.int
         if v < 0: v += T.umod.int
         return T(v.uint32)
+  template initModInt*(v: SomeInteger or ModInt; M: static[int] = 1_000_000_007): auto =
+    StaticModInt[M].init(v)
 
+# TODO
 #  converter toModInt[M:static[int]](n:SomeInteger):ModInt[M] {.inline.}= initModInt(n, M)
 
   proc initModIntRaw*(v: SomeInteger; M: static[int] = 1_000_000_007): auto {.inline.} =
@@ -71,18 +64,12 @@ when not defined ATCODER_MODINT_HPP:
       u -= t * v;swap(u, v)
     return T.init(u)
 
-#  proc retake*[M: static[int]](m: var ModInt[M]) {.inline.} =
-#    int(m) = int(m).modulo(M)
-
   proc val*(m: ModInt): int {.inline.} =
     int(m)
 
-#  proc modulo*[M: static](m: ModInt): int {.inline.} =
-#    M
-
-  proc `-`*(m: ModInt): ModInt {.inline.} =
+  proc `-`*[T:ModInt](m: T): T {.inline.} =
     if int(m) == 0: return m
-    else: return m.type(m.umod() - int(m))
+    else: return T(m.umod() - uint32(m))
 
   template generateDefinitions(name, l, r, body: untyped): untyped {.dirty.} =
     proc name*[T:ModInt](l: T; r: T): auto {.inline.} =
@@ -91,9 +78,6 @@ when not defined ATCODER_MODINT_HPP:
       body
     proc name*[T:ModInt](l: T; r: SomeInteger): auto {.inline.} =
       body
-
-#  proc inv*[M: static[int]](m: ModInt[M]): ModInt[M] {.inline.} =
-#    result = initModInt(extgcd(M, int(m))[1], M)
 
   proc `+=`*[T:ModInt](m: var T; n: SomeInteger | T) {.inline.} =
     uint32(m) += T.init(n).uint32
@@ -110,11 +94,9 @@ when not defined ATCODER_MODINT_HPP:
       uint32(m) = Barrett_of_DynamicModInt.mul(uint(m), T.init(n).uint).uint32
     else:
       static: assert false
-#    m.retake()
 
   proc `/=`*[T:ModInt](m: var T; n: SomeInteger | T) {.inline.} =
     uint32(m) = (uint(m) * T.init(n).inv().uint mod T.umod).uint32
-#    m.retake()
 
   generateDefinitions(`+`, m, n):
     result = T.init(m)
