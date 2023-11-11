200984fe3241dbd2ffc9c2006f869455d8fc2526
Toshihiro Shimizu <toshihiroshimizu@ToshihironoMac-mini.local>
2023-11-11 15:33:02 +0900

    特性多項式のテストを追加

---
+++ src/verify/extra/math/yosupo_determinant_test.nim
@@ -0,0 +1,18 @@
+# verify-helper: PROBLEM https://judge.yosupo.jp/problem/matrix_det
+
+include atcoder/header
+import atcoder/modint
+import atcoder/extra/math/matrix
+
+type mint = modint998244353
+
+block main:
+  let N = nextInt()
+  type Mat = DynamicMatrixType(mint)
+  var
+    A = Mat.init(N)
+  for i in 0 ..< N:
+    for j in 0 ..< N:
+      A[i, j] = nextInt()
+
+  echo determinant(A)
