ac7f2b31878bae2ebbd1a475447789be42ef4f6b
E49869826 <154519054+E49869826@users.noreply.github.com>
2025-04-07 16:37:52 +0000

    修正漏れを修正
    while の部分も修正する必要がある

--- src/atcoder/extra/other/binary_search.nim
+++ src/atcoder/extra/other/binary_search.nim
@@ -4,7 +4,7 @@ when not declared ATCODER_BINARY_SEARCH_HPP:
   proc minLeft*(f:proc(x:int):bool, s:Slice[int]):int =
     var (l, r) = (s.a - 1, s.b)
     if not f(r): return s.b + 1
-    while r - l > 1:
+    while r > l + 1:
       let d = cast[int]((cast[uint](r) - cast[uint](l)) shr 1)
       let m = l + d
       if f(m): r = m
@@ -13,7 +13,7 @@ when not declared ATCODER_BINARY_SEARCH_HPP:
   proc maxRight*(f:proc(x:int):bool, s:Slice[int]):int =
     var (l, r) = (s.a, s.b + 1)
     if not f(l): return s.a - 1
-    while r - l > 1:
+    while r - 1 > l:
       let d = cast[int]((cast[uint](r) - cast[uint](l)) shr 1)
       let m = l + d
       if f(m): l = m
