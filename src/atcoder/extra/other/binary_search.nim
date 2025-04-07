4f6f79d115cb5008b913f27a77f8e79f522d906a
E49869826 <154519054+E49869826@users.noreply.github.com>
2025-04-07 16:26:57 +0000

    Fix binary_search.nim
    l = -5*10^18, r = 7*10^18 といったときにオーバーフローするコードになっていたので修正

--- src/atcoder/extra/other/binary_search.nim
+++ src/atcoder/extra/other/binary_search.nim
@@ -5,7 +5,7 @@ when not declared ATCODER_BINARY_SEARCH_HPP:
     var (l, r) = (s.a - 1, s.b)
     if not f(r): return s.b + 1
     while r - l > 1:
-      let d = (r - l) shr 1
+      let d = cast[int]((cast[uint](r) - cast[uint](l)) shr 1)
       let m = l + d
       if f(m): r = m
       else: l = m
@@ -14,7 +14,7 @@ when not declared ATCODER_BINARY_SEARCH_HPP:
     var (l, r) = (s.a, s.b + 1)
     if not f(l): return s.a - 1
     while r - l > 1:
-      let d = (r - l) shr 1
+      let d = cast[int]((cast[uint](r) - cast[uint](l)) shr 1)
       let m = l + d
       if f(m): l = m
       else: r = m
