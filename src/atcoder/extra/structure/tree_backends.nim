b695e98d70fc333a8a56e98db288aa6ff6840865
Toshihiro Shimizu <toshihiroshimizu@ToshihironoMac-mini.local>
2026-07-07 10:22:12 +0900

    Document tree backend usage

---
+++ src/atcoder/extra/structure/tree_backends.nim
@@ -0,0 +1,10 @@
+when not declared ATCODER_EXTRA_STRUCTURE_TREE_BACKENDS_HPP:
+  const ATCODER_EXTRA_STRUCTURE_TREE_BACKENDS_HPP* = 1
+
+  import atcoder/extra/structure/red_black_tree
+  import atcoder/extra/structure/randomized_binary_search_tree_with_parent
+  import atcoder/extra/structure/splay_tree
+
+  export red_black_tree
+  export randomized_binary_search_tree_with_parent
+  export splay_tree
