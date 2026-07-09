bf5c7462f6f86b115860f68beff6b6d38014406c
Toshihiro Shimizu <toshihiroshimizu@ToshihironoMac-mini.local>
2026-07-09 16:06:36 +0900

    Improve dijkstra radix heap tests and docs

--- tests/test_extra_graph_shortest_paths.nim
+++ tests/test_extra_graph_shortest_paths.nim
@@ -4,6 +4,7 @@ when not declared ATCODER_TEST_EXTRA_GRAPH_SHORTEST_PATHS:
   import std/unittest
   import atcoder/extra/graph/graph_template
   import atcoder/extra/graph/dijkstra
+  import atcoder/extra/graph/dijkstra_radix_heap
   import atcoder/extra/graph/bellman_ford
   import atcoder/extra/graph/warshall_floyd
 
@@ -37,6 +38,41 @@ when not declared ATCODER_TEST_EXTRA_GRAPH_SHORTEST_PATHS:
       check d[1] == 0
       check d[2] == 0
       check d[3] == 1
+
+    test "dijkstra_radix_heap handles integer nonnegative weights":
+      var g = initGraph(5)
+      g.addEdge(0, 1, 2)
+      g.addEdge(0, 2, 9)
+      g.addEdge(1, 2, 1)
+      g.addEdge(1, 3, 6)
+      g.addEdge(2, 3, 1)
+      g.addEdge(3, 4, 3)
+      g.addEdge(1, 4, 20)
+
+      var d = dijkstra_radix_heap(g, 0)
+
+      check d.contains(4)
+      check d[0] == 0
+      check d[1] == 2
+      check d[2] == 3
+      check d[3] == 4
+      check d[4] == 7
+      check d.path(4) == @[0, 1, 2, 3, 4]
+
+    test "dijkstra_radix_heap supports multiple sources":
+      var g = initGraph(5)
+      g.addEdge(0, 2, 10)
+      g.addEdge(1, 2, 3)
+      g.addEdge(2, 3, 4)
+      g.addEdge(4, 3, 1)
+
+      var d = dijkstra_radix_heap(g, @[0, 1, 4])
+
+      check d[0] == 0
+      check d[1] == 0
+      check d[2] == 3
+      check d[3] == 1
+      check d[4] == 0
 
     test "bellman_ford handles negative edges without negative cycle":
       var g = initGraph(4)
